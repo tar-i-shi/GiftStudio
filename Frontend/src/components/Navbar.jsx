164deddca39847132a3da58f2b8cbe95c129e5ec
tarishi <hemanitarishi@gmail.com>
2026-04-05 05:35:56 +0530

    frontend changes

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -64,7 +64,7 @@ export default function Navbar() {
                     <Link to="/common-gifts">Gifts</Link>
                     <Link to="/personalised-products">Personalised</Link>
                     <Link to="/occasions">Occasions</Link>
-                    <li><Link to="/contact">Contact</Link></li>
+                    <Link to="/contact">Contact</Link>
                 </div>
 
                 <div className="flex items-center space-x-4">
